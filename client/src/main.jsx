fb2c52ade7a2980596446700ed2f05288dc6737a
Emmanuel Ngetich <engetich333@gmail.com>
2026-08-04 12:59:51 +0300

    Setup frontend architecture and routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/sidebar.css";
+import "./styles/topbar.css";
+import "./styles/buttons.css";
+import "./styles/cards.css";
+import "./styles/tables.css";
+import "./styles/forms.css";
+import "./styles/dashboard.css";
+import "./styles/login.css";
+import "./styles/animations.css";
+import "./styles/responsive.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
